f4f0f75039c8507069ac7683a4398dadbdb78ef5
maheshkumar24424 <150494068+maheshkumar24424@users.noreply.github.com>
2024-05-25 19:44:48 -0400

    Added second printing stuff

--- src/test/java/DevTest.java
+++ src/test/java/DevTest.java
@@ -2,5 +2,6 @@ public class DevTest {
     public static void main(String[] args) {
         System.out.println("Hello Dev Test");
         System.out.println("Printing stuff");
+        System.out.println("Printing stuff 2");
     }
 }
